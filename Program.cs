92bd53cafad4b8aa6b1395fa9c754228119494dd
Marius <marius_taboo@yahoo.com>
2018-07-24 18:03:14 +0100

    Migrate to dotnet core 2.1

--- Program.cs
+++ Program.cs
@@ -16,13 +16,11 @@ namespace BetSystem
 
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseUrls(Urls)
-                .Build();
+                .UseStartup<Startup>();
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,9 @@ using AutoMapper;
 using BetSystem.Controllers.Resources;
 using BetSystem.Repository;
 using BetSystem.Helper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace BetSystem
 {
@@ -36,7 +39,23 @@ namespace BetSystem
 
             services.AddDbContext<BetDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
 
-            services.AddMvc();
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddHsts(options =>
+            {
+                options.Preload = true;
+                options.IncludeSubDomains = true;
+                options.MaxAge = TimeSpan.FromDays(60);
+                options.ExcludedHosts.Add("example.com");
+                options.ExcludedHosts.Add("www.example.com");
+            });
+
+            services.AddHttpsRedirection(options =>
+            {
+                options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+                options.HttpsPort = 5050;
+            });        
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,13 +68,14 @@ namespace BetSystem
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
             }
 
             // Seed Currency Table by default
             seeder.Seed();
 
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
-
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseMvc(routes =>
